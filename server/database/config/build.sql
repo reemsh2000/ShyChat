4e8963f66abd77ea598e0960de4f3a8b1bc73a2c
reemsh2000 <sh951652@gmail.com>
2021-11-23 10:34:34 +0200

    create tables

---
+++ server/database/config/build.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users,chat,chatParticipent CASCADE;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    phone VARCHAR(50) UNIQUE NOT NULL,
+    password TEXT,
+    photo TEXT,
+    bio TEXT,
+    status boolean  DEFAULT FALSE
+);
+CREATE TABLE chat(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+CREATE TABLE chatParticipent(
+    id SERIAL PRIMARY KEY,
+    userId INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    roomId INTEGER REFERENCES chat(id) ON DELETE CASCADE 
+);
+
+COMMIT;
