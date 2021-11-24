4043dabc219a436ea546b44bff3cd81047dbc019
muhammadharoun <hro19502001@gmail.com>
2021-11-24 14:50:03 +0200

    add next to function

--- server/controllers/utilities/twilio.js
+++ server/controllers/utilities/twilio.js
@@ -1,26 +1,21 @@
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-const sendSMS = async (req, res) => {
+const sendSMS = async (req, res, next) => {
   const verification = await client.verify.services(process.env.TWILIO_VERIFY_SERVICE_SID)
     .verifications.create({ to: `${req.body.phone}`, channel: 'sms' });
   if (verification.status === 'pending') {
-    res.status(200).json({ message: 'Verification code sent' });
-  } else {
+    next();
     res.status(400).json({ message: 'Error sending verification code' });
   }
 };
 
-const verifySMS = async (req, res) => {
+const verifySMS = async (req, res, next) => {
   const verification = await client.verify.services(process.env.TWILIO_VERIFY_SERVICE_SID)
     .verificationChecks.create({ to: `${req.body.phone}`, code: req.body.code });
   if (verification.status === 'approved') {
-    res.json({
-      success: true,
-      message: 'Verification successful',
-    });
+    next();
   } else {
-    res.json({
-      success: false,
+    res.status(400).json({
       message: 'Verification failed',
     });
   }
