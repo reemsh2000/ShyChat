3802ddb4c308aece351d0c79576f435ef90ab15a
muhammadharoun <hro19502001@gmail.com>
2021-11-24 14:31:04 +0200

    convert function to async await

--- server/controllers/utilities/twilio.js
+++ server/controllers/utilities/twilio.js
@@ -1,37 +1,29 @@
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-const sendSMS = (req, res) => {
-  client.verify.services(process.env.TWILIO_VERIFY_SERVICE_SID)
-    .verifications.create({ to: `${req.body.phone}`, channel: 'sms' })
-    .then((verification) => {
-      res.json({
-        success: true,
-        message: `Verification code sent to ${verification.to}`,
-        code: verification.status,
-      });
-    }).catch((err) => {
-      res.json({
-        success: false,
-        message: err.message,
-      });
-    });
+const sendSMS = async (req, res) => {
+  const verification = await client.verify.services(process.env.TWILIO_VERIFY_SERVICE_SID)
+    .verifications.create({ to: `${req.body.phone}`, channel: 'sms' });
+  if (verification.status === 'pending') {
+    res.status(200).json({ message: 'Verification code sent' });
+  } else {
+    res.status(400).json({ message: 'Error sending verification code' });
+  }
 };
 
-const verifySMS = (req, res) => {
-  client.verify.services(process.env.TWILIO_VERIFY_SERVICE_SID)
-    .verificationChecks.create({ to: `${req.body.phone}`, code: req.body.code })
-    .then((verification) => {
-      res.json({
-        success: true,
-        message: `Verification code sent to ${verification.to}`,
-        code: verification.status,
-      });
-    }).catch((err) => {
-      res.json({
-        success: false,
-        message: err.message,
-      });
+const verifySMS = async (req, res) => {
+  const verification = await client.verify.services(process.env.TWILIO_VERIFY_SERVICE_SID)
+    .verificationChecks.create({ to: `${req.body.phone}`, code: req.body.code });
+  if (verification.status === 'approved') {
+    res.json({
+      success: true,
+      message: 'Verification successful',
     });
+  } else {
+    res.json({
+      success: false,
+      message: 'Verification failed',
+    });
+  }
 };
 
 module.exports = { sendSMS, verifySMS };
