37d97937859519eb6a1400f5f0665d4846ccee31
reemsh2000 <sh951652@gmail.com>
2021-11-26 22:23:57 +0200

    socket settings in backend

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,16 +1,35 @@
 const http = require('http');
-const cors = require('cors');
 const { Server } = require('socket.io');
 const app = require('../app');
+const createChat = require('../controllers/chat');
+// const addNewMessage = require('..cle/database/queries/chat/addNewMessage');
 
 const server = http.createServer(app);
-
-app.use((cors));
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
   },
 });
 io.on('connection', (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+  // console.log(`User Connected: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data);
+    // console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on('send_message', async (data) => {
+    // console.log(data);
+    const { senderId, receiverPhone, room } = data;
+    await createChat(senderId, receiverPhone, socket);
+    console.log('id',socket.chatId);
+    socket.to(room).emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    // console.log('User Disconnected', socket.id);
+  });
 });
+
+module.exports = server;
