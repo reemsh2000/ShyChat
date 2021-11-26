47fb533cedef35313d2327d0db5f2d1209c45286
reemsh2000 <sh951652@gmail.com>
2021-11-26 22:54:51 +0200

    create chat function

---
+++ server/controllers/chat/index.js
@@ -0,0 +1,22 @@
+const {
+  addNewParticipant, checkExistChat, createNewChat, checkPhoneQuery,
+} = require('../../database/queries');
+
+const createChat = async (senderId, receiverPhone, socket) => {
+  try {
+    const { rows: userChats, rowCount } = await checkExistChat(senderId, receiverPhone);
+    if (rowCount) {
+      socket.chatId = userChats[0].chatid;
+    } else {
+      const { rows: userInfo } = await checkPhoneQuery(receiverPhone);
+      const { rows: chatInfo } = await createNewChat();
+      socket.chatId = chatInfo[0].id;
+      await addNewParticipant(senderId, chatInfo[0].id);
+      await addNewParticipant(userInfo[0].id, chatInfo[0].id);
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+};
+module.exports = createChat;
