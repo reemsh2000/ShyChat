a65167783d8e0cf0e03031d8e753f6751dc695bf
Nizar7zak <nizar7zakout@gmail.com>
2021-11-27 12:11:34 +0200

    edit login schema

--- server/controllers/utilities/serverSideValidation/loginSchema.js
+++ server/controllers/utilities/serverSideValidation/loginSchema.js
@@ -1,7 +1,7 @@
 const joi = require('joi');
 
 const loginSchema = joi.object({
-  phoneNumber: joi.string().length(10).pattern(/^[0-9]+$/).required(),
+  phoneNumber: joi.string().min(8).required(),
   password: joi.string().min(5).required(),
 });
 
