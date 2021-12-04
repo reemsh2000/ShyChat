6fa72848020128ca84b3797409a23723dabf2d05
reemsh2000 <sh951652@gmail.com>
2021-12-04 22:31:00 +0200

    handle login

--- client/src/App/ProtectedRoute.tsx
+++ client/src/App/ProtectedRoute.tsx
@@ -4,13 +4,15 @@ import { Route, Redirect } from 'react-router-dom';
 interface ProtectedRouteProps {
   children: any;
   isLogged: boolean;
+  path:string;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
   children,
   isLogged,
+  path,
 }) => (isLogged ? (
-  <Route exact path="/">
+  <Route exact path={path}>
     {children}
   </Route>
 ) : (
